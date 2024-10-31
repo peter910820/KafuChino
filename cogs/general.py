07096155551823fcd57fb5a47e44592a3cf729b6
peter910820 <peter910820@gmail.com>
2024-10-31 22:12:22 +0800

    Build architecture

---
+++ cogs/general.py
@@ -0,0 +1,21 @@
+import discord
+
+from discord import app_commands
+from discord.ext import commands
+
+
+class General(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @app_commands.command(name="ping", description="return bot delay")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"delay time: {str(round(self.latency*1000, 2))}ms.")
+
+    @app_commands.command(name="id", description="check user id.")
+    async def id(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Your user ID: {interaction.user.id}")
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(General(bot), guild=None)
---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+class KafuChino(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='$',
+            intents=intents
+        )
+
+    async def setup_hook(self):
+        await self.load_extension('cogs.general')
+        await bot.tree.sync(guild=None)
+
+    async def on_ready(self):
+        delay_time = str(round(self.latency*1000, 2))
+        print(f'{self.user} is starting......')
+        print(f'{self.user} is online. delay time: {delay_time}ms.')
+        status = discord.Activity(
+            type=discord.ActivityType.watching, name='ご注文はうさぎですか？')
+        await self.change_presence(status=discord.Status.online, activity=status)
+
+
+bot = KafuChino()
+bot.run(os.getenv('BOT_TOKEN'))
