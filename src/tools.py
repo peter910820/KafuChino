1f5d451f773c11380198cac00661b9b3da8af251
peter910820 <peter910820@gmail.com>
2024-11-03 02:00:28 +0800

    regex change forbidden_char, add youtube_palyer_output(make some command to embed)

--- src/tools.py
+++ src/tools.py
@@ -6,3 +6,10 @@ async def error_output(error_message):
         title='ERROR', description='An error has occurred', color=discord.Color.red())
     embed.add_field(name='Error content', value=error_message)
     return embed
+
+
+async def youtube_palyer_output(youtube_palyer_message):
+    embed = discord.Embed(
+        title='YT-PLAYER', description='youtube_palyer message', color=discord.Color.dark_red())
+    embed.add_field(name='Notice', value=youtube_palyer_message)
+    return embed
