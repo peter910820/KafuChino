027b238eba31e55635cda3d7c67c5b9e359290c9
peter910820 <peter910820@gmail.com>
2024-11-02 18:24:50 +0800

    cancel manage command develop

--- cogs/manage.py
+++ cogs/manage.py
@@ -8,41 +8,29 @@ class Manage(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="reload", description="reload cogs(for develop)")
-    async def reload(self, interaction: discord.Interaction, extension_name: str):
+    @app_commands.command(name='cog', description='Manage cogs(for develop)')
+    @app_commands.describe(action_name='The action to manage cog.')
+    @app_commands.choices(action_name=[
+        app_commands.Choice(name='load', value='load'),
+        app_commands.Choice(name='unload', value='unload'),
+        app_commands.Choice(name='reload', value='reload'),
+    ])
+    async def reload(self, interaction: discord.Interaction, action_name: str, extension_name: str):
         if str(interaction.user.id) != str(self.bot.owner_id):
-            await interaction.response.send_message("Only bot owner can use this command!", ephemeral=True)
+            await interaction.response.send_message('Only bot owner can use this command!', ephemeral=True)
             return
         try:
-            await self.bot.reload_extension(f"cogs.{extension_name}")
+            match action_name:
+                case 'load':
+                    await self.bot.load_extension(f'cogs.{extension_name}')
+                case 'unload':
+                    await self.bot.unload_extension(f'cogs.{extension_name}')
+                case 'reload':
+                    await self.bot.reload_extension(f'cogs.{extension_name}')
             await self.bot.tree.sync()
-            await interaction.response.send_message(f"successful reload {extension_name}", ephemeral=True)
+            print(f'successful {action_name} {extension_name}')
         except Exception as e:
-            await interaction.response.send_message(f"reload {extension_name} failed\nerror: {e}", ephemeral=True)
-
-    @app_commands.command(name="unload", description="unload cogs(for develop)")
-    async def unload(self, interaction: discord.Interaction, extension_name: str):
-        if str(interaction.user.id) != str(self.bot.owner_id):
-            await interaction.response.send_message("Only bot owner can use this command!", ephemeral=True)
-            return
-        try:
-            await self.bot.load_extension(f"cogs.{extension_name}")
-            await self.bot.tree.sync()
-            await interaction.response.send_message(f"successful unload {extension_name}", ephemeral=True)
-        except Exception as e:
-            await interaction.response.send_message(f"unload {extension_name} failed\nerror: {e}", ephemeral=True)
-
-    @app_commands.command(name="load", description="load cogs(for develop)")
-    async def load(self, interaction: discord.Interaction, extension_name: str):
-        if str(interaction.user.id) != str(self.bot.owner_id):
-            await interaction.response.send_message("Only bot owner can use this command!", ephemeral=True)
-            return
-        try:
-            await self.bot.load_extension(f"cogs.{extension_name}")
-            await self.bot.tree.sync()
-            await interaction.response.send_message(f"successful load {extension_name}", ephemeral=True)
-        except Exception as e:
-            await interaction.response.send_message(f"load {extension_name} failed\nerror: {e}", ephemeral=True)
+            print(f'{action_name} {extension_name} failed\nerror: {e}')
 
 
 async def setup(bot: commands.Bot) -> None:
