9f4cd9cd631c2ed357504fc2424cd1f512e3d393
peter910820 <peter910820@gmail.com>
2024-11-03 02:55:04 +0800

    fix logger display and skip func

--- src/tools.py
+++ src/tools.py
@@ -3,13 +3,13 @@ import discord
 
 async def error_output(error_message):
     embed = discord.Embed(
-        title='ERROR', description='An error has occurred', color=discord.Color.red())
+        title='ERROR', description='An error has occurred', color=discord.colour.Colour.magenta())
     embed.add_field(name='Error content', value=error_message)
     return embed
 
 
-async def youtube_palyer_output(youtube_palyer_message):
+async def notice_output(youtube_palyer_message):
     embed = discord.Embed(
-        title='YT-PLAYER', description='youtube_palyer message', color=discord.Color.dark_red())
+        title='YT-PLAYER', description='youtube_palyer message', color=discord.colour.Colour.blurple())
     embed.add_field(name='Notice', value=youtube_palyer_message)
     return embed
