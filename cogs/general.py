adb294edad254d7642a4ccf7ec5308ea68f004e1
peter910820 <peter910820@gmail.com>
2024-11-02 18:36:09 +0800

    add clean func

--- cogs/general.py
+++ cogs/general.py
@@ -8,13 +8,13 @@ class General(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="ping", description="return bot delay")
+    @app_commands.command(name='ping', description='return bot delay')
     async def ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"delay time: {str(round(self.bot.latency*1000, 2))}ms.")
+        await interaction.response.send_message(f'delay time: {str(round(self.bot.latency*1000, 2))}ms.')
 
-    @app_commands.command(name="id", description="check user id.")
+    @app_commands.command(name='id', description='check user id.')
     async def id(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"Your user ID: {interaction.user.id}")
+        await interaction.response.send_message(f'Your user ID: {interaction.user.id}')
 
 
 async def setup(bot: commands.Bot) -> None:
